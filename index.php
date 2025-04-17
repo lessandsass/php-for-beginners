19a0687136a3a9f0e6f61cfb2f9bcbbbfd788630
Vipu <lessandsass@gmail.com>
2025-04-17 13:28:57 +0530

    Add index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Demo</title>
+
+    <style>
+        body {
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+            font-size: 2rem;
+        }
+    </style>
+
+</head>
+<body>
+
+    <?php
+        $name = "Dark Matter";
+        $read = true;
+
+        if ($read) {
+            $message = "You have read $name";
+        }
+
+    ?>
+
+    <h1>
+        <?php echo $message; ?>
+    </h1>
+    
+</body>
+</html>
