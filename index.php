402ed2115cd5c66651fe98522f24cb13a88b2a7a
Vipu <lessandsass@gmail.com>
2025-04-18 17:14:19 +0530

    filterByAuthor function was implemented

--- index.php
+++ index.php
@@ -19,21 +19,51 @@
             ],
             [
                 'name' => 'The Hobbit',
-                'author' => 'J.R.R. Tolkien',
+                'author' => 'J.R.R. Vincent Tolkien',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/2.The_Hobbit',
-            ]
+            ],
+            [
+                'name' => 'The Two Towers',
+                'author' => 'J.R.R. Crowley Tolkien',
+                'purchaseUrl' => 'https://www.goodreads.com/book/show/3.The_Two_Towers',
+            ],
+            [
+                'name' => 'The Return of the King',
+                'author' => 'J.R.R. Tolkien',
+                'purchaseUrl' => 'https://www.goodreads.com/book/show/4.The_Return_of_the_King',
+            ],
+            [
+                'name' => 'The Silmarillion',
+                'author' => 'J.R.R. Tolkien',
+                'purchaseUrl' => 'https://www.goodreads.com/book/show/5.The_Silmarillion',
+            ],
         ];
+
+        function filterByAuthor($books, $author) 
+        {
+            $filteredBooks = [];
+
+            foreach ($books as $book) {
+                if ($book['author'] === $author) {
+                    $filteredBooks[] = $book;
+                }
+            }
+            return $filteredBooks;
+        }
+
     ?>
 
     <ul>
-        <?php foreach ($books as $book) : ?>
+        <?php foreach (filterByAuthor($books, 'J.R.R. Vincent Tolkien') as $book) : ?>
             <li>
-                <a href="<?= $book['purchaseUrl'] ?>">
+                <a 
+                    href="<?= $book['purchaseUrl'] ?>"
+                >
                     <?= $book['name'] ?> by <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach ?>
     </ul>
 
-</body>
+</body> 
 </html>
