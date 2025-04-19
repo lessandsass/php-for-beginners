844db4e361baec0c2b64ff66a1ab932f02cbba80
Vipu <lessandsass@gmail.com>
2025-04-19 06:15:06 +0530

    Change the filterByAuthor function to able to change any kind of parameter

--- index.php
+++ index.php
@@ -16,45 +16,52 @@
                 'name' => 'The Lord of the Rings',
                 'author' => 'J.R.R. Tolkien',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/1.The_Lord_of_the_Rings',
+                'releaseYear' => 1954,
             ],
             [
                 'name' => 'The Hobbit',
                 'author' => 'J.R.R. Vincent Tolkien',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/2.The_Hobbit',
+                'releaseYear' => 1937,
             ],
             [
                 'name' => 'The Two Towers',
                 'author' => 'J.R.R. Crowley Tolkien',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/3.The_Two_Towers',
+                'releaseYear' => 1954,
             ],
             [
                 'name' => 'The Return of the King',
                 'author' => 'J.R.R. Tolkien',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/4.The_Return_of_the_King',
+                'releaseYear' => 1955,
             ],
             [
                 'name' => 'The Silmarillion',
-                'author' => 'J.R.R. Tolkien',
+                'author' => 'V.Vipurthanan',
                 'purchaseUrl' => 'https://www.goodreads.com/book/show/5.The_Silmarillion',
+                'releaseYear' => 1977,
             ],
         ];
 
-        function filterByAuthor($books, $author) 
+        function filter($items, $key, $value) 
         {
-            $filteredBooks = [];
+            $filteredItems = [];
 
-            foreach ($books as $book) {
-                if ($book['author'] === $author) {
-                    $filteredBooks[] = $book;
+            foreach ($items as $item) {
+                if ($item[$key] === $value) {
+                    $filteredItems[] = $item;
                 }
             }
-            return $filteredBooks;
+            return $filteredItems;
         }
+
+        $filteredBooks = filter($books, 'releaseYear', 1977);
 
     ?>
 
     <ul>
-        <?php foreach (filterByAuthor($books, 'J.R.R. Vincent Tolkien') as $book) : ?>
+        <?php foreach ($filteredBooks as $book) : ?>
             <li>
                 <a 
                     href="<?= $book['purchaseUrl'] ?>"
