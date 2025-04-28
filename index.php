8f0c775525f75407944ba0d89b0e4e97517db825
Vipu <lessandsass@gmail.com>
2025-04-28 19:11:46 +0530

    Specific function to check the active links

--- index.php
+++ index.php
@@ -2,4 +2,15 @@
 
 $heading = "Home Page";
 
+function dd() {
+    echo "<pre>";
+    var_dump(func_get_args());
+    echo "</pre>";
+    die();
+}
+
+function urlIs($url) {
+    return $_SERVER['REQUEST_URI'] === $url;
+}
+
 require "views/index.view.php";
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -33,7 +33,7 @@
                 </div>
                 <div class="hidden sm:ml-6 sm:block">
                     <div class="flex space-x-4">
-                        <a href="/" class="<?= $_SERVER['REQUEST_URI'] === "/" ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">
+                        <a href="/" class="<?= urlIs('/') ?  "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" aria-current="page">
                             Home
                         </a>
                         
