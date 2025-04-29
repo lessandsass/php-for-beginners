215ae967ad1876a828383bcb8c743efea7ae652b
Vipu <lessandsass@gmail.com>
2025-04-29 12:25:48 +0530

    OOP introduction

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
 
 require 'functions.php'; 
-require 'router.php';
+// require 'router.php';
 
+// connect to the MySQL database
+// initialize the PDO
+// create a instance of PDO
+
+class Person 
+{
+    public $name;
+    public $age;
+
+    public function breath()
+    {
+        echo $this->name . ' is breathing';
+    }
+}
+
+$person = new Person();
+$person->name = 'John';
+$person->age = 25;
+// dd($person);
+
+// dd($person->name);
+// dd($person->age);
+$person->breath();
