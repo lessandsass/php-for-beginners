d2f6319ad2524c1acdadde2f388aecc752b3aaed
Vipu <lessandsass@gmail.com>
2025-04-29 10:10:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,10 +10,15 @@ $routes = [
     '/contact' => 'controllers/contact.php',
 ];
 
+function abort($code = 404) 
+{
+    http_response_code($code);
+    require 'views/404.php';
+    die();    
+}
+
 if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
-    http_response_code(404);
-    require 'views/404.php';
-    die();
+    abort();
 }
