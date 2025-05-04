aa468d78521fe4a186eb57ec16262e1843ea9842
Vipu <lessandsass@gmail.com>
2025-05-04 16:33:06 +0530

    Database.php

---
+++ Database.php
@@ -0,0 +1,30 @@
+<?php
+
+class Database {
+
+    public $connection;
+
+    public function __construct() 
+    {
+        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8";
+        $this->connection = new PDO($dsn, 'root', '');
+    }
+
+    public function query($query) 
+    {
+        $statement = $this->connection->prepare($query);
+
+        $statement->execute();
+
+        return $statement;
+
+    }
+}
+
+
+
+
+
+
+
+
--- index.php
+++ index.php
@@ -1,33 +1,7 @@
 <?php
 
 require 'functions.php'; 
-// require 'router.php';
-
-// connect to the MySQL database
-// initialize the PDO
-// create a instance of PDO
-// $dsn = Data Source Name
-
-class Database {
-
-    public $connection;
-
-    public function __construct() 
-    {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8";
-        $this->connection = new PDO($dsn, 'root', '');
-    }
-
-    public function query($query) 
-    {
-        $statement = $this->connection->prepare($query);
-
-        $statement->execute();
-
-        return $statement;
-
-    }
-}
+require 'Database.php';
 
 $db = new Database();
 
