377bb91c055d95af1954e05e1875417670f3bea4
Vipu <lessandsass@gmail.com>
2025-05-08 17:13:06 +0530

    retrive as assoc, changed to db connection from fetchAll function

--- Database.php
+++ Database.php
@@ -6,11 +6,29 @@ class Database {
 
     public function __construct() 
     {
-        $dsn = "mysql:host=localhost;port=3306;dbname=myapp;charset=utf8";
-        $this->connection = new PDO($dsn, 'root', '');
+
+        $config = [
+            'host' => 'localhost',
+            'port' => '3306',
+            'dbname' => 'myapp',
+            'charset' => 'utf8'
+        ];
+
+        http_build_query($data);  
+
+        $dsn = "
+                    mysql:host={$config['host']};
+                    port={$config['port']};
+                    dbname={$config['dbname']};
+                    charset={$config['charset']}
+                ";
+
+        $this->connection = new PDO($dsn, 'root', '', [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
 
-    public function query($query) 
+    public function query($query)  
     {
         $statement = $this->connection->prepare($query);
 
--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require 'Database.php';
 
 $db = new Database();
 
-$posts = $db->query('SELECT * FROM posts')->fetchAll(PDO::FETCH_ASSOC);
+$posts = $db->query('SELECT * FROM posts')->fetchAll();
 
 dd($posts);
 
