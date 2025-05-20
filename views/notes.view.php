4caff3da5364778ef6386450abf69d045d8fa860
Vipu <lessandsass@gmail.com>
2025-05-20 17:56:58 +0530

    The notes page had been created

---
+++ views/notes.view.php
@@ -0,0 +1,16 @@
+<?php require "partials/head.php"; ?>
+<?php require "partials/nav.php"; ?>
+<?php  require "partials/banner.php"; ?>
+
+
+<section>
+    <h1>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold">Notes Page</h1>
+    </div>
+    </h1>
+</section>
+
+<?php require "partials/footer.php"; ?>
+
+
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -41,6 +41,10 @@
                             About
                         </a>
 
+                        <a href="/notes" class="<?= urlIs('/notes') ?  "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium">
+                            Notes
+                        </a>
+
                         <a href="/contact" class="<?= urlIs('/contact') ?  "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white">
                             Contact
                         </a>
