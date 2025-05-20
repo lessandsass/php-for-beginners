c5b0811fda597de82a7cb593c96f2587ba315059
Vipu <lessandsass@gmail.com>
2025-05-20 19:12:36 +0530

    Query for the user_id of 3 in the notes controller

--- controllers/notes.php
+++ controllers/notes.php
@@ -1,6 +1,11 @@
 <?php
 
-$heading = "Notes Page";
+$config = require('config.php');
+$db = new Database($config['database']);
+
+$heading = "My Notes";
+
+$notes = $db->query('SELECT * FROM notes WHERE user_id = 3;')->fetchAll();
 
 require "views/notes.view.php";
 
