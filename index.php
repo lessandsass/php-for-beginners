712644694cb801aed8c41874c22a8c9c94b57aca
Vipu <lessandsass@gmail.com>
2025-05-20 17:51:54 +0530

    the router file was pulled in the index.php file

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'functions.php'; 
+require 'router.php';
 require 'Database.php';
 
 $config = require('config.php');
@@ -8,10 +9,9 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $id = $_GET['id'];
+$query = "SELECT * FROM posts where id = :id";
 
-$query = "SELECT * FROM posts where id = ?";
-
-$posts = $db->query($query, [$id])->fetch();
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 dd($posts);
 
