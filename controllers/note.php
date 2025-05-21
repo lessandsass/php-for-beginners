1b363a152ecc571679222524025c475b6ede394b
Vipu <lessandsass@gmail.com>
2025-05-21 13:18:45 +0530

    Update SQL query to prevent unauthrized access

--- controllers/note.php
+++ controllers/note.php
@@ -5,7 +5,14 @@ $db = new Database($config['database']);
 
 $heading = "The Note";
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes WHERE user_id = :user_id AND id = :id', [
+    'id' => $_GET['id'],
+    'user_id' => 3
+])->fetch();
+
+if (!$note) {
+    abort(404);
+}
 
 require "views/note.view.php";
 
