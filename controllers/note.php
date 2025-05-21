293c6484f18bdb17afff911fc25d5da15caffda0
Vipu <lessandsass@gmail.com>
2025-05-21 16:30:54 +0530

    apply the findOrFail method

--- controllers/note.php
+++ controllers/note.php
@@ -6,12 +6,7 @@ $db = new Database($config['database']);
 $heading = "The Note";
 $currentUserId = 3;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->find();
-
-if (! $note) {
-    abort(404);
-}
-
+$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
 
 if ($note['user_id'] !== $currentUserId) {
     abort(Response::FORBIDDEN);
