5f18626d20bf10ace86ee397e2773348607d799a
Vipu <lessandsass@gmail.com>
2025-05-21 15:03:24 +0530

    The abort function was updated

--- router.php
+++ router.php
@@ -22,7 +22,7 @@ function routeToController($uri, $routes)
 function abort($code = 404) 
 {
     http_response_code($code);
-    require 'views/404.php';
+    require 'views/' . $code . '.php';
     die();    
 }
 
