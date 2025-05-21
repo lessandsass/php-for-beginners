facdc8fc97d359408ead10d7fc7ccc8235eaf71b
Vipu <lessandsass@gmail.com>
2025-05-21 15:07:40 +0530

    403 file was created

---
+++ views/403.php
@@ -0,0 +1,14 @@
+<?php require ('partials/head.php'); ?>
+<?php  require "partials/nav.php"; ?>
+
+<section>
+    <div class="flex flex-col items-center justify-center h-screen">
+        <h1 class="text-4xl font-bold">Unauthorized</h1>
+        <p class="mt-4">
+            <a href="/" class="text-blue-500 p-3 font-bold text-lg">Back to Home</a>
+        </p>
+    </div>
+</section>
+
+<?php require ('partials/footer.php');?>
+
