ed87a0aed7b3e83a9ac7fbff838ab3476e07c867
Vipu <lessandsass@gmail.com>
2025-05-21 16:16:37 +0530

    use the find method instead of fetch method

--- controllers/note.php
+++ controllers/note.php
@@ -4,14 +4,14 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = "The Note";
+$currentUserId = 3;
 
-$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->find();
 
 if (! $note) {
     abort(404);
 }
 
-$currentUserId = 3;
 
 if ($note['user_id'] !== $currentUserId) {
     abort(Response::FORBIDDEN);
