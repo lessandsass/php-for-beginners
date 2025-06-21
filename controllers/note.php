f94d18bf90cb0b2081b4860acac2cac68f09c744
Vipu <lessandsass@gmail.com>
2025-06-21 10:25:34 +0530

    authorize function

--- controllers/note.php
+++ controllers/note.php
@@ -4,13 +4,11 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = "The Note";
-$currentUserId = 3;
+$currentUserId = 3; 
 
 $note = $db->query('SELECT * FROM notes WHERE id = :id', ['id' => $_GET['id']])->findOrFail();
 
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] !== $currentUserId);
 
 require "views/note.view.php";
 
