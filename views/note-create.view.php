f6f73b45f0ebdd8d3d62cc0999f7df907dda7c7b
Vipu <lessandsass@gmail.com>
2025-07-16 19:39:54 +0530

    alternative way to display the errors

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -24,12 +24,17 @@
                                         rows="3" 
                                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" 
                                         placeholder="Here's as an idea for a note..."
-                                        required
                                     ></textarea>
 
-                                    <?php if (!empty($errors)) : ?>
+                                    <!-- <?php if (!empty($errors)) : ?>
                                         <div class="mt-2 text-sm text-red-600">
                                             <p><?= $errors[0]; ?></p>
+                                        </div>
+                                    <?php endif; ?> -->
+
+                                    <?php if (isset($errors['body'])) : ?>
+                                        <div class="mt-2 text-sm text-red-600">
+                                            <p><?= $errors['body']; ?></p>
                                         </div>
                                     <?php endif; ?>
 
