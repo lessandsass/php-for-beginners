73ae5ccf23cdee73931d3529afa4e5af4fd73bdd
Vipu <lessandsass@gmail.com>
2025-07-16 19:22:20 +0530

    avoid displaying malisious input

--- views/note.view.php
+++ views/note.view.php
@@ -8,7 +8,7 @@
         <div class="flex flex-col items-center justify-center h-screen">
 
             <p><a href="/notes" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded block mb-4">Go back</a></p>
-            <p><?= $note['body']; ?></p>
+            <p><?= htmlspecialchars($note['body']); ?></p>
 
         </div>
     </h1>
--- views/notes.view.php
+++ views/notes.view.php
@@ -10,10 +10,10 @@
             <ul>
                 <?php foreach ($notes as $note) : ?>
                     <a href="/note?id=<?= $note['id']; ?>" class="text-blue-500 hover:underline">
-                        <li><?= $note['body']; ?></li>
+                        <li><?= htmlspecialchars($note['body']); ?></li>
                     </a>
                 <?php endforeach; ?> 
-            </ul>
+            </ul>   
 
             <p class="mt-6">
                 <a href="/note/create" class="text-blue-500 hover:underline">Create a new note</a>
