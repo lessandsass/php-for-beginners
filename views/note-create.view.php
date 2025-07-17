4938fd3aebd792a4cd6e4657656fcdc7dae5e849
Vipu <lessandsass@gmail.com>
2025-07-17 05:52:28 +0530

    Adding the values after submit the form to show, if any errors happened

--- views/note-create.view.php
+++ views/note-create.view.php
@@ -24,8 +24,7 @@
                                         rows="3" 
                                         class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" 
                                         placeholder="Here's as an idea for a note..."
-                                        required
-                                    ></textarea>
+                                    ><?= $_POST['body'] ?? ''; ?></textarea>
 
                                     <?php if (!empty($errors)) : ?>
                                         <div class="mt-2 text-sm text-red-600">
