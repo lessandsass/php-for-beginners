3015f1aef804fa18a1d7b689df65864653adec60
Vipu <lessandsass@gmail.com>
2025-08-15 20:56:43 +0530

    request methods
    - - Add all the methods get, post, delete, patch and put

--- Core/router.php
+++ Core/router.php
@@ -1,23 +1,83 @@
 <?php
 
+namespace Core;
 
-function routeToController($uri, $routes)
-{
-    if (array_key_exists($uri, $routes)) {
-        require base_path($routes[$uri]);
-    } else {
-        abort();
+class Router {
+    protected $routes = [];
+
+    public function get($uri, $controller) 
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'GET',
+        ];
     }
+
+    public function post($uri, $controller) 
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'POST',
+        ];
+    }
+
+    public function delete($uri, $controller) 
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'DELETE',
+        ];
+    }
+
+    public function patch($uri, $controller) 
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PATCH',
+        ];
+    }
+
+    public function put($uri, $controller) 
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'controller' => $controller,
+            'method' => 'PUT',
+        ];
+    }
+
+    public function route($uri)
+    {
+        foreach ($this->routes as $route) {
+            if ($route['uri'] === $uri) {
+                require base_path($route['controller']);
+            }
+        }
+
+        // abort();
+    }
+
 }
 
-function abort($code = 404) 
-{
-    http_response_code($code);
-    require base_path("views/{$code}.php");
-    die();    
-}
+// function routeToController($uri, $routes)
+// {
+//     if (array_key_exists($uri, $routes)) {
+//         require base_path($routes[$uri]);
+//     } else {
+//         abort();
+//     }
+// }
 
-$routes = require base_path('routes.php');
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-routeToController($uri, $routes);
+// function abort($code = 404) 
+// {
+//     http_response_code($code);
+//     require base_path("views/{$code}.php");
+//     die();    
+// }
+
+
 
