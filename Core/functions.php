b1685e7671559dad6703956718cb3ae50c45c5ac
Vipu <lessandsass@gmail.com>
2025-08-15 22:12:20 +0530

    abort function
    - The abort function was created

--- Core/functions.php
+++ Core/functions.php
@@ -15,6 +15,13 @@ function urlIs($url)
     return $_SERVER['REQUEST_URI'] === $url;
 }
 
+function abort($code = 404) 
+{
+    http_response_code($code);
+    require base_path("views/{$code}.php");
+    die();    
+}
+
 function authorize($condition, $status = Response::FORBIDDEN)
 {
     if (!$condition) {
